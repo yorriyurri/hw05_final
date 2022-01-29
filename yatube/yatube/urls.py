ee11182a3b17b72f77f1ea67309966205e30c64f
Юрий Ивайкин <skierorama@gmail.com>
2022-01-29 18:15:18 +0300

    ivaikin_final_3.1

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
 ]
 
 handler404 = 'core.views.page_not_found'
-handler403 = 'core.views.handler403'
+handler403 = 'core.views.csrf_failure'
 handler500 = 'core.views.error_500'
 
 if settings.DEBUG:
